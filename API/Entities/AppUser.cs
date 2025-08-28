2beb3ee2c7ed2fc4b92c4d77c0ab589983cfd014
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-08-28 08:28:47 -0600

    first commit

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.Entities
+{
+    public class AppUser
+    {
+        public required string Id { get; set; } = Guid.NewGuid().ToString();
+        public required string DisplayName { get; set; } = "iVAN IsrAEL";
+        public required string Email { get; set; }
+    }
+}
