602fd08f2e370cd252c7eec10e617dbcbe7a242b
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-09-03 10:01:54 -0600

    Parcial01 - Add Angular Skeleton and Http Client to SPA

---
+++ client/src/app/app.ts
@@ -0,0 +1,23 @@
+import { Component, signal, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App implements OnInit{
+  private http = inject(HttpClient);
+  protected readonly title = signal('Dating App');
+
+  ngOnInit(): void {
+      this.http.get('https://localhost:7031/api/members').subscribe({
+          next: response => console.log(response),
+          error: error => console.log(error),
+          complete: () => console.log("Completed the http request")
+        }
+      )
+  }
+}
