babda9e07a0097e76938513de9f63500400fdee9
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-09-10 07:25:20 -0600

    Parcial02 - Add Base Api controller

--- API/Controllers/MembersController.cs
+++ API/Controllers/MembersController.cs
@@ -1,7 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
@@ -9,9 +5,7 @@ using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class MembersController(AppDbContext context) : ControllerBase // inyección
+    public class MembersController(AppDbContext context) : BaseApiController // inyección
     {
 
         [HttpGet]
