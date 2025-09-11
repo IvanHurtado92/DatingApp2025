51008ee4b0087d8c5e820f67f9d0f2b786eabcbf
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-09-11 07:26:52 -0600

    Parcial02 - Add AccountController and DTO

---
+++ API/DTOs/RegisterRequest.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.DTOs
+{
+    public class RegisterRequest
+    {
+        [Required]
+        public required string DisplayName { get; set; } = "iVAN IsrAEL";
+        [Required]
+        [EmailAddress]
+        public required string Email { get; set; }
+        [Required]
+        [MinLength(6)]
+        public required string Password { get; set; }
+    }
+}
