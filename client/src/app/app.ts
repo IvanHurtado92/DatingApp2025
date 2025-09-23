53a63767d9bc1427700919c7f61286673a8f5338
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-09-23 15:04:24 -0600

    Parcial02 - Add Persistence to Login User with Signals

--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -2,28 +2,37 @@ import { Component, signal, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { lastValueFrom } from 'rxjs';
-import { Nav } from "../layout/nav/nav";
+import { Nav } from '../layout/nav/nav';
+import { AccountService } from '../core/services/account-service';
 
 @Component({
   selector: 'app-root',
   imports: [Nav],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
-export class App implements OnInit{
+export class App implements OnInit {
+  private accountService = inject(AccountService);
   private http = inject(HttpClient);
   protected readonly title = signal('Dating App');
   protected members = signal<any>([]);
 
   async ngOnInit(): Promise<void> {
-      this.members.set(await this.getMembers())
+    this.setCurrentUser();
+    this.members.set(await this.getMembers());
   }
 
-  async getMembers(): Promise<Object>{
-    try{
-      return lastValueFrom(this.http.get('https://localhost:7031/api/members'))
-    }
-    catch(error){
+  setCurrentUser() {
+    const userString = localStorage.getItem('user');
+    if (!userString) return;
+    const user = JSON.parse(userString);
+    this.accountService.currentUser.set(user);
+  }
+
+  async getMembers(): Promise<Object> {
+    try {
+      return lastValueFrom(this.http.get('https://localhost:7031/api/members'));
+    } catch (error) {
       console.log(error);
       throw error;
     }
