ca003e342ddeaf6fea5b6cf863646d2491e99852
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-09-25 07:42:15 -0600

    Parcial02 - Add Child to Parent Communication

--- client/src/features/home/home.ts
+++ client/src/features/home/home.ts
@@ -12,7 +12,7 @@ export class Home {
   @Input({required: true}) membersFromApp: User[] = []; //Capaz de recibir parametro de entrada
   protected registerMode = signal(false);
 
-  showRegister(): void {
-    this.registerMode.set(true);
+  showRegister(value: boolean): void {
+    this.registerMode.set(value);
   }
 }
