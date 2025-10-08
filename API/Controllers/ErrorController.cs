ded1fcb9ce4abc9562f6bcd6b9938f2ae2f372d7
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-10-08 15:50:05 -0600

    Parcial02 - Middleware Corrected

--- API/Controllers/ErrorController.cs
+++ API/Controllers/ErrorController.cs
@@ -1,35 +1,34 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 
-namespace API.Controllers
+namespace API.Controllers;
+
+public class ErrorController : BaseApiController
 {
-    public class ErrorController : BaseApiController
+
+    [HttpGet("bad-request")]
+    public IActionResult GetBadRequest() // 400
     {
-        [HttpGet("bad-request")]
-        public IActionResult GetBadRequest() //400
-        {
-            return BadRequest("Bad request");
-        }
+        var inputParam = -1;
+        if (inputParam <= 0) throw new ArgumentOutOfRangeException(nameof(inputParam));
+        
+        return BadRequest("Bad request");
+    }
 
-        [HttpGet("auth")]
-        public IActionResult GetAuth() //401
-        {
-            return Unauthorized();
-        }
+    [HttpGet("auth")]
+    public IActionResult GetAuth() // 401
+    {
+        return Unauthorized();
+    }
 
-        [HttpGet("not-found")]
-        public IActionResult GetNotFound()
-        {
-            return NotFound();
-        }
+    [HttpGet("not-found")]
+    public IActionResult GetNotFound() // 404
+    {
+        return NotFound();
+    }
 
-        [HttpGet("server-error")]
-        public IActionResult GerServerError()
-        {
-            throw new Exception("Server error");
-        }
+    [HttpGet("server-error")]
+    public IActionResult GetServerError() // 500
+    {
+        throw new Exception("Server error");
     }
 }
--- API/Middlewares/ExceptionMiddleware.cs
+++ API/Middlewares/ExceptionMiddleware.cs
@@ -1,39 +1,50 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text.Json;
-using System.Threading.Tasks;
 using API.Exceptions;
 
-namespace API.Middlewares
+namespace API.Middlewares;
+
+public class ExceptionMiddleware(
+    RequestDelegate next,
+    ILogger<ExceptionMiddleware> logger,
+    IHostEnvironment env)
 {
-    public class ExceptionMiddleware(
-        RequestDelegate next,
-        ILogger<ExceptionMiddleware> Logger,
-        IHostEnvironment env)
+    public async Task InvokeAsync(HttpContext context)
     {
-        public async Task InvokeAsync(HttpContext context)
+        try
         {
-            try
-            {
-                await next(context);
-            }
-            catch (Exception ex)
-            {
-                Logger.LogError(ex, "{message}", ex.Message);
-                context.Response.ContentType = "application/json";
-                context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+            await next(context);
+        }
+        catch (ArgumentOutOfRangeException ex)
+        {
+            logger.LogError(ex, "{message}", ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.BadRequest;
 
-                var response = env.IsDevelopment() ?
-                    new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace) :
-                    new ApiException(context.Response.StatusCode, ex.Message, "Internal Server Error");
+            var response = env.IsDevelopment() ?
+                new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace) :
+                new ApiException(context.Response.StatusCode, ex.Message, "The provided params are incorrect");
 
-                var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
-                var json = JsonSerializer.Serialize(response, option);
+            var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = JsonSerializer.Serialize(response, option);
 
-                await context.Response.WriteAsync(json);
-            }
+            await context.Response.WriteAsync(json);
+        }
+        catch (Exception ex)
+        {
+            logger.LogError(ex, "{message}", ex.Message);
+            context.Response.ContentType = "application/json";
+            context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
+
+            var response = env.IsDevelopment() ?
+                new ApiException(context.Response.StatusCode, ex.Message, ex.StackTrace) :
+                new ApiException(context.Response.StatusCode, ex.Message, "Internal server error");
+
+            var option = new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase };
+            var json = JsonSerializer.Serialize(response, option);
+
+            await context.Response.WriteAsync(json);
         }
     }
+
 }
