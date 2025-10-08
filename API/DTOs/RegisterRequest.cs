a58f834f4c28f63ef415755704d6b0362ab4444a
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-10-08 15:49:37 -0600

    Parcial02 - Parcial01 Register DTO Fixed

--- API/DTOs/RegisterRequest.cs
+++ API/DTOs/RegisterRequest.cs
@@ -1,20 +1,15 @@
-using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
-namespace API.DTOs
+namespace API.DTOs;
+
+public class RegisterRequest
 {
-    public class RegisterRequest
-    {
-        [Required]
-        public required string DisplayName { get; set; } = "iVAN IsrAEL";
-        [Required]
-        [EmailAddress]
-        public required string Email { get; set; }
-        [Required]
-        [MinLength(6)]
-        public required string Password { get; set; }
-    }
+    [Required]
+    public string DisplayName { get; set; } = string.Empty;
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; } = string.Empty;
+    [Required]
+    [MinLength(6)]
+    public string Password { get; set; } = string.Empty;
 }
