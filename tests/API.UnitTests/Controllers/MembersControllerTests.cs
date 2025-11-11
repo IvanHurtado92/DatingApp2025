3bc8ad6b7a8185330f4dad57c8be91481c5ac19a
Ivan Hurtado <ivan.hurtado92@outlook.com>
2025-11-10 19:26:00 -0600

    Parcial03 - Add more unit tests

--- tests/API.UnitTests/Controllers/MembersControllerTests.cs
+++ tests/API.UnitTests/Controllers/MembersControllerTests.cs
@@ -1,78 +1,164 @@
 using System.Security.Claims;
-using System.Threading.Tasks;
 using API.Controllers;
 using API.Entities;
 using API.Interfaces;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
-using NUnit.Framework;
+using NSubstitute.ReturnsExtensions;
 
-namespace API.UnitTests.Controllers
+namespace API.UnitTests.Controllers;
+
+public class MembersControllerTests
 {
-    public class MembersControllerTests
+    private MembersController _membersController;
+    private IMembersRepository _mockMembersRepository;
+
+    [SetUp]
+    public void Setup()
     {
-        private MembersController _membersController;
-        private IMembersRepository _mockMembersRepository;
+        _mockMembersRepository = Substitute.For<IMembersRepository>();
+        _membersController = new MembersController(_mockMembersRepository);
+    }
 
-        [SetUp]
-        public void Setup()
+    [Test]
+    public async Task GetMembers_Valid_ReturnMembers()
+    {
+        // Arrange
+        var userId = "userId";
+        DefaultHttpContext testHttpContext = new()
         {
-            _mockMembersRepository = Substitute.For<IMembersRepository>();
-            _membersController = new MembersController(_mockMembersRepository);
-        }
-
-        [Test]
-        public async Task GetMembers_Valid_ReturnMembers()
+            User = new ClaimsPrincipal(new ClaimsIdentity([
+                new Claim("email", userId)
+            ]))
+        };
+        _membersController.ControllerContext = new ControllerContext
         {
-            // Enfoque de la triple A en pruebas unitarias
+            HttpContext = testHttpContext
+        };
 
-            // Arrange
-            var userId = "userID";
-            DefaultHttpContext textHttpContext = new DefaultHttpContext
+        IReadOnlyList<Member> expectedMembers =
+        [
+            new Member
             {
-                User = new ClaimsPrincipal(new ClaimsIdentity([
-                    new Claim("email", userId)
-                ]))
-            };
+                Id = "test-id",
+                BirthDay = DateOnly.Parse("2000-01-01"),
+                ImageUrl = null,
+                DisplayName = "Test",
+                Created = DateTime.UtcNow,
+                LastActive = DateTime.UtcNow,
+                Gender = "Gender",
+                Description = "Description",
+                City = "City",
+                Country = "Country",
+                User = null!,
+                Photos = []
+            }
+        ];
 
-            _membersController.ControllerContext = new ControllerContext();
-            _membersController.ControllerContext.HttpContext = textHttpContext;
+        _mockMembersRepository.GetMembersAsync().Returns(expectedMembers);
 
-            IReadOnlyList<Member> expectedMembers =
-            [
-                new Member
-                {
-                    Id = "test-id",
-                    BirthDay = DateOnly.Parse("2000-01-01"),
-                    ImageUrl = null,
-                    DisplayName = "Test",
-                    Created = DateTime.UtcNow,
-                    LastActive = DateTime.UtcNow,
-                    Gender = "Gender",
-                    Description = "Description",
-                    City = "",
-                    Country = "Country",
-                    User = null!,
-                }
-            ];
+        // Act & Assert
+        var membersResult = await _membersController.GetMembers();
+        var okResult = membersResult.Result as OkObjectResult;
+        Assert.That(okResult, Is.Not.Null, "Expected OkObjectResult but got something else");
 
-            _mockMembersRepository.GetMembersAsync().Returns(expectedMembers);
+        var members = okResult.Value as IReadOnlyList<Member>;
+        Assert.That(members, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(members, Has.Count.EqualTo(1));
+        });
+    }
 
-            // Act
-            var membersResult = await _membersController.GetMembers();
-            var okResult = membersResult.Result as OkObjectResult;
-            
-            // Assert
+    [Test]
+    public async Task GetMember_Valid_ReturnMembers()
+    {
+        // Arrange
+        var userId = "userId";
+        DefaultHttpContext testHttpContext = new()
+        {
+            User = new ClaimsPrincipal(new ClaimsIdentity([
+                new Claim("email", userId)
+            ]))
+        };
+        _membersController.ControllerContext = new ControllerContext
+        {
+            HttpContext = testHttpContext
+        };
 
-            Assert.That(okResult, Is.Not.Null, "Expected OkObjectResult but got something else");
+        Member expectedMember = new()
+        {
+            Id = "test-id",
+            BirthDay = DateOnly.Parse("2000-01-01"),
+            ImageUrl = null,
+            DisplayName = "Test",
+            Created = DateTime.UtcNow,
+            LastActive = DateTime.UtcNow,
+            Gender = "Gender",
+            Description = "Description",
+            City = "City",
+            Country = "Country",
+            User = null!,
+            Photos = []
+        };
 
-            var members = okResult.Value as IReadOnlyList<Member>;
-            Assert.That(members, Is.Not.Null);
-            Assert.Multiple(() =>
-            {
-                Assert.That(members.Count, Is.EqualTo(1)); 
-            });
-        }
+        _mockMembersRepository.GetMemberAsync(expectedMember.Id).Returns(expectedMember);
+
+        // Act
+        var memberResult = await _membersController.GetMember(expectedMember.Id);
+        var member = memberResult.Value;
+
+        // Assert
+        Assert.That(member, Is.Not.Null);
+        Assert.Multiple(() =>
+        {
+            Assert.That(member.Id, Is.EqualTo(expectedMember.Id));
+            Assert.That(member.BirthDay, Is.EqualTo(DateOnly.Parse("2000-01-01")));
+            Assert.That(member.City, Is.EqualTo("City"));
+        });
+    }
+
+    [Test]
+    public async Task GetMember_Valid_ReturnNotFound()
+    {
+        // Arrange
+        var userId = "userId";
+        DefaultHttpContext testHttpContext = new()
+        {
+            User = new ClaimsPrincipal(new ClaimsIdentity([
+                new Claim("email", userId)
+            ]))
+        };
+        _membersController.ControllerContext = new ControllerContext
+        {
+            HttpContext = testHttpContext
+        };
+
+        Member expectedMember = new()
+        {
+            Id = "test-id",
+            BirthDay = DateOnly.Parse("2000-01-01"),
+            ImageUrl = null,
+            DisplayName = "Test",
+            Created = DateTime.UtcNow,
+            LastActive = DateTime.UtcNow,
+            Gender = "Gender",
+            Description = "Description",
+            City = "City",
+            Country = "Country",
+            User = null!,
+            Photos = []
+        };
+
+        _mockMembersRepository.GetMemberAsync(expectedMember.Id).ReturnsNull();
+
+        // Act & Assert
+        var memberResult = await _membersController.GetMember(expectedMember.Id);
+        var notFoundResult = memberResult.Result as NotFoundObjectResult;
+        Assert.That(notFoundResult, Is.Not.Null, "Expected NotFoundObjectResult but got something else");
+
+        var member = memberResult.Value;
+        Assert.That(member, Is.Null);
     }
 }
